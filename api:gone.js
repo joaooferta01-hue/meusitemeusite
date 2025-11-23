d775ea65ad076e6bdff79120481b5e2fdf967778
joaooferta01-hue <joaooferta01@gmail.com>
2025-11-23 13:16:33 -0300

    Add files via upload

---
+++ api:gone.js
@@ -0,0 +1,26 @@
+{
+  "enableAnrDetection" : false,
+  "timestamp" : 1679514704213,
+  "autoScreenshot" : false,
+  "enableJsAgent" : true,
+  "flushInterval" : 300,
+  "enableAnrStackTrace" : false,
+  "enableCrashReporting" : true,
+  "enableScreenshot" : false,
+  "enableJsAgentSPA" : true,
+  "anrThreshold" : 3000,
+  "screenshotUseCellular" : false,
+  "deviceMetricsConfigurations" : {
+    "criticalMemoryThresholdPercentage" : 90,
+    "criticalBatteryThresholdPercentage" : 90,
+    "collectionFrequencyMins" : 2,
+    "criticalStorageThresholdPercentage" : 90,
+    "enableStorage" : true,
+    "enableMemory" : true,
+    "enableBattery" : false
+  },
+  "enableFeatures" : [
+
+  ],
+  "enableJsAgentAjax" : false
+}
